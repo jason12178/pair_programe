2bebf9b33770054b59cf581f7cded50e250ece8c
Calvin <chaucheukmancalvin@gmail.com>
2020-04-09 11:12:47 +0800

    final version

--- testcase.py
+++ testcase.py
@@ -10,10 +10,14 @@ class TestStringMethods(unittest.TestCase):
         husband_tax = tax(Separate(husband_income))
         wife_tax = tax(Separate(wife_income))
         joint_tax = tax(joint(husband_income,wife_income))
-
-        self.assertEqual(wife_tax, 0)
-        self.assertEqual(husband_tax, 9420)
-        self.assertEqual(joint_tax, 420)
+        husband_MPF=MPF(husband_income)
+        wife_MPF=MPF(wife_income)
+        
+        self.assertEqual(husband_MPF,15000,"should be 15000")
+        self.assertEqual(wife_MPF,0, "should be 0")
+        self.assertEqual(wife_tax, 0, "should be 0")
+        self.assertEqual(husband_tax, 9420, "should be 9420")
+        self.assertEqual(joint_tax, 420, "should be 420")
 
     def testcase2(self):
         
@@ -22,10 +26,46 @@ class TestStringMethods(unittest.TestCase):
         husband_tax = tax(Separate(husband_income))
         wife_tax = tax(Separate(wife_income))
         joint_tax = tax(joint(husband_income,wife_income))
+        husband_MPF=MPF(husband_income)
+        wife_MPF=MPF(wife_income)
 
-        self.assertEqual(wife_tax, 41500)
-        self.assertEqual(husband_tax, 0)
-        self.assertEqual(joint_tax, 19060)
+        self.assertEqual(husband_MPF,0, "should be 0")
+        self.assertEqual(wife_MPF,18000, "should be 18000")
+        self.assertEqual(wife_tax, 41500, "should be 41500")
+        self.assertEqual(husband_tax, 0, "should be 0")
+        self.assertEqual(joint_tax, 19060, "should be 19060")
+
+    def testcase3(self):
+        
+        husband_income = 500000
+        wife_income = 500000
+        husband_tax = tax(Separate(husband_income))
+        wife_tax = tax(Separate(wife_income))
+        joint_tax = tax(joint(husband_income,wife_income))
+        husband_MPF=MPF(husband_income)
+        wife_MPF=MPF(wife_income)
+
+        self.assertEqual(husband_MPF,18000, "should be 18000")
+        self.assertEqual(wife_MPF,18000, "should be 18000")
+        self.assertEqual(wife_tax, 41500, "should be 41500")
+        self.assertEqual(husband_tax, 41500, "should be 41500")
+        self.assertEqual(joint_tax, 101000, "should be 101000")
+
+    def testcase4(self):
+        
+        husband_income = 100000
+        wife_income = 200000
+        husband_tax = tax(Separate(husband_income))
+        wife_tax = tax(Separate(wife_income))
+        joint_tax = tax(joint(husband_income,wife_income))
+        husband_MPF=MPF(husband_income)
+        wife_MPF=MPF(wife_income)
+
+        self.assertEqual(husband_MPF,5000, "should be 5000")
+        self.assertEqual(wife_MPF,10000, "should be 10000")
+        self.assertEqual(wife_tax, 1480, "should be 1480")
+        self.assertEqual(husband_tax, 0, "should be 0")
+        self.assertEqual(joint_tax, 420, "should be 420")
 
 if __name__ == '__main__':
     unittest.main()
